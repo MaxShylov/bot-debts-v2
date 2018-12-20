f3459b6a55776d8194ddc2e019bb1903c5ceb9f6
MaxShylov <shma9013@gmai.com>
2018-12-20 04:39:30 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const config = require('config');
+const config = require('./config');
 
 const TelegramBot = require('node-telegram-bot-api');
 
