34d3a6659658062ab6a667f302e34a2b4d07a981
Max Shylov <shma9013@gmail.com>
2020-02-19 02:00:43 +0200

    adds error to web command getDebts when chatId is not exist and refactoring

--- server/commandsWeb/getDebts.js
+++ server/commandsWeb/getDebts.js
@@ -1,15 +1,14 @@
 const DebtsModel = require('../db/models/debts.model');
+const { errorMessage } = '../helpers/common';
 
 const getDebts = async (req, res) => {
   const { chatId } = req.query;
+  const errorResponse = (msg) => res.status(500).send(errorMessage(msg));
+
+  if (!chatId) return errorResponse('chatId is not exist');
+
   const data = await DebtsModel.find({ chatId }, err => {
-    if (err)
-      return res.status(500).send(
-        JSON.stringify({
-          status: 'error',
-          error: JSON.stringify(err.message),
-        }),
-      );
+    if (err) return errorResponse(err.message);
   })
     .lean()
     .exec();
